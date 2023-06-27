7a86aa676b13f46468484210513227ea51624b6e
Janavi Vekariya <janavi764@gmail.com>
2023-06-27 22:27:38 +0530

    api

---
+++ book-app/backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+
+// Endpoint for searching books
+app.get('/search', (req, res) => {
+    const searchQuery = req.query.q; // Get the search query from the request query parameters
+  
+    // Make a GET request to the Open Library API
+    axios.get(`https://openlibrary.org/search.json?q=${searchQuery}`)
+      .then(response => {
+        const books = response.data.docs.map(book => {
+          return {
+            title: book.title,
+            author: book.author_name ? book.author_name[0] : 'Unknown',
+            cover: `http://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`,
+            description: book.first_publish_year ? `First published in ${book.first_publish_year}` : 'No publication year available'
+          };
+        });
+        res.json(books); // Send the book search results as JSON response
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while searching for books.' });
+      });
+  });
+  
+  // Endpoint for adding a book to the readlist
+  app.post('/readlist', (req, res) => {
+    const book = req.body; // Assuming the request body contains the book object to be added to the readlist
+  
+    // Save the book to the readlist (you can use a database or any other storage mechanism)
+    // For simplicity, we'll assume there's an array to store the readlist
+    readlist.push(book);
+  
+    res.status(201).json({ message: 'Book added to readlist successfully.' });
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
