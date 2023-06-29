4a3149c5551b4769654cdac8c7974ee28e9c545e
KathleenX7 <kxiong1210@gmail.com>
2023-06-29 01:06:36 -0400

    Update ReadingBook.jsx

--- book-app/src/components/ReadingPage/ReadingBook.jsx
+++ book-app/src/components/ReadingPage/ReadingBook.jsx
@@ -1,32 +1,44 @@
 import {useState, useEffect} from "react";
 import bookImage from "../../assets/book title.jpg";
 
-const ReadingBook = () => {
+const ReadingBook = ({title, author, year, description, image}) => {
     const [makeSmaller, setSmaller] = useState(false);
     useEffect(() => {
+        let x = Math.floor((Math.random() * 2) + 1);
+        if(x == 1){
+            setClass("divBook divBookPurple");
+        }else{
+            setClass("divBook");
+        }
+        if(window.innerWidth < 990){
+            setSmaller("");
+        }
+        
         window.addEventListener("resize", () => {
             if(window.innerWidth < 990){
-                setSmaller(true);
+                setSmaller("");
             }else{
-                setSmaller(false);
+                setSmaller("divBookLarger");
             }
         });
     }, []);
     
     return (
         <>
-        <div className = {makeSmaller? "divBook": "divBook divBookLarger"}>
+        <div className = {divClass + " " + makeSmaller}>
             <div className = "subDivBookImg"> 
-                <img src = {bookImage} alt = "book image"></img>
+                <img src = {image} alt = "book image"></img>
             </div>
             <div className = "subDivBook"> 
-                <h6>Harry Potter and the Philosopher's Stone</h6> {/* add the link to the title */}
+                <h6>{title} </h6> {/* add the link to the title */}
                 <div className = "newLine">
-                    <p className = "leftAlign" >Author</p>
-                    <p className = "rightAlign" >Year</p>
+                    <p className = "leftAlign" >{author}</p>
+                    <p className = "rightAlign" >{year}</p>
                 </div>
 
-                <p>This book is about a lot of content that is content. I am not writing nonsense so that this is about the length of a description. We also might need to consider if the description is too long, but lets get the formatting in first. Do you think this is long enought. lets see</p>
+                {/* <button onClick = {handleMoreInfo} className = "subDivBook-clearButton"><u>More Info</u></button>
+                
+                <p>{description}</p> */}
             </div>
         </div>
         </>
