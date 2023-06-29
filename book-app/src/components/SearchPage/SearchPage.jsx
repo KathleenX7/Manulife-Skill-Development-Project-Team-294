7871feda2545c4ce6f61d8df45b6d5e0b475523f
KathleenX7 <kxiong1210@gmail.com>
2023-06-28 23:14:59 -0400

    Update SearchPage.jsx

--- book-app/src/components/SearchPage/SearchPage.jsx
+++ book-app/src/components/SearchPage/SearchPage.jsx
@@ -6,7 +6,7 @@ function SearchPage() {
         <div style={{ textAlign: 'center' }}>
             <p style={{ fontSize: '80px' }}>Bookpedia</p>
             <SearchBar/>
-            <BookList/>
+            <Link to="/booklist" className="btn btn-primary">Book List</Link>
         </div>
     )
 };
