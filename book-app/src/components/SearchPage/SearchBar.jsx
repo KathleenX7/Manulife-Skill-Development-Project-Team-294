35b8931f6a3f54630135a7c8da336be1387aa588
KathleenX7 <kxiong1210@gmail.com>
2023-06-28 23:17:15 -0400

    fix

--- book-app/src/components/SearchPage/SearchBar.jsx
+++ book-app/src/components/SearchPage/SearchBar.jsx
@@ -8,9 +8,9 @@ const SearchBar = () => {
     return (
         <div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <input type = "Author" className = "searchInput" style={{ textAlign: 'center'}} type = "text" placeholder="search by author" value = {author} ></input>
-                <input type = "Subject" className = "searchInput" style={{ textAlign: 'center'}} type = "text" placeholder="search by title" value = {title}></input>
-                <input type = "General Information" className = "searchInput" style={{ textAlign: 'center'}} type = "text" placeholder="search by description" value = {description}></input>
+                <input type = "Author" className = "searchInput" style={{ textAlign: 'center'}} placeholder="search by author" value = {author} ></input>
+                <input type = "Subject" className = "searchInput" style={{ textAlign: 'center'}} placeholder="search by title" value = {title}></input>
+                <input type = "General Information" className = "searchInput" style={{ textAlign: 'center'}} placeholder="search by description" value = {description}></input>
                 <button> Search by author </button>
                 <button> Search by subject </button>
                 <button> Search by general </button>
