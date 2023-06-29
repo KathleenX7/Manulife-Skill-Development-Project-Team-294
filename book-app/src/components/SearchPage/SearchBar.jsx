9697a4302f77e2949fdceceabb9891e2d710fe22
KathleenX7 <kxiong1210@gmail.com>
2023-06-28 23:26:06 -0400

    Update SearchBar.jsx

--- book-app/src/components/SearchPage/SearchBar.jsx
+++ book-app/src/components/SearchPage/SearchBar.jsx
@@ -4,13 +4,36 @@ const SearchBar = () => {
     const [author, setAuthor] = useState("");
     const [subject, setSubject] = useState("");
     const [general, setGeneral] = useState("");
+    const handleChangeAuthor = (event) => {
+        setAuthors(event.target.value);
+    }
+    const handleSearchAuthor = () => {
+        
+        setAuthors("");
+    }
 
+    const handleChangeSubject = (event) => {
+        setSubject(event.target.value);
+    }
+    const handleSearchSubject = () => {
+        
+        setSubject("");
+    }
+
+    const handleChangeGeneral = (event) => {
+        setGeneral(event.target.value);
+    }
+    const handleSearchGeneral = () => {
+        
+        setGeneral("");
+    }
+    
     return (
         <div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <input type = "Author" className = "searchInput" style={{ textAlign: 'center'}} placeholder="search by author" value = {author} ></input>
-                <input type = "Subject" className = "searchInput" style={{ textAlign: 'center'}} placeholder="search by title" value = {title}></input>
-                <input type = "General Information" className = "searchInput" style={{ textAlign: 'center'}} placeholder="search by description" value = {description}></input>
+                <input type = "Subject" className = "searchInput" style={{ textAlign: 'center'}} placeholder="search by subject" value = {subject}></input>
+                <input type = "General Information" className = "searchInput" style={{ textAlign: 'center'}} placeholder="search by description" value = {general}></input>
                 <button> Search by author </button>
                 <button> Search by subject </button>
                 <button> Search by general </button>
